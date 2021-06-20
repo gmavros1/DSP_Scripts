1d73c4325c48dfcb37f7b9563f9be9d3b7af118c
gmavros1 <gmavrosi@csd.auth.gr>
2021-06-20 15:59:11 +0300

    fin non linear

--- EEG_Non_Linear_Dynamics/eeg_analysis.m
+++ EEG_Non_Linear_Dynamics/eeg_analysis.m
@@ -37,34 +37,49 @@ end
 
 % Predictability of the EEG-trace - approximateEntropy
 
-aE1 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 1)),eLAG,eDIM);
-aE2 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 2)),eLAG,eDIM);
-aE3 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 3)),eLAG,eDIM);
-aE4 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 4)),eLAG,eDIM);
-aE5 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 5)),eLAG,eDIM);
+aE1 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 1)),eLAG1,eDIM1);
+aE2 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 2)),eLAG2,eDIM2);
+aE3 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 3)),eLAG3,eDIM3);
+aE4 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 4)),eLAG4,eDIM4);
+aE5 = approximateEntropy((filteredSignal(sensor,1:10*Fs, 5)),eLAG5,eDIM5);
 
 % level of chaotic complexity in the EEG-trace - correlationDimension
 
-cDim1 = correlationDimension((filteredSignal(sensor,1:10*Fs, 1)),eLAG,eDIM);
-cDim2 = correlationDimension((filteredSignal(sensor,1:10*Fs, 2)),eLAG,eDIM);
-cDim3 = correlationDimension((filteredSignal(sensor,1:10*Fs, 3)),eLAG,eDIM);
-cDim4 = correlationDimension((filteredSignal(sensor,1:10*Fs, 4)),eLAG,eDIM);
-cDim5 = correlationDimension((filteredSignal(sensor,1:10*Fs, 5)),eLAG,eDIM);
+cDim1 = correlationDimension((filteredSignal(sensor,1:10*Fs, 1)),eLAG1,eDIM1);
+cDim2 = correlationDimension((filteredSignal(sensor,1:10*Fs, 2)),eLAG2,eDIM2);
+cDim3 = correlationDimension((filteredSignal(sensor,1:10*Fs, 3)),eLAG3,eDIM3);
+cDim4 = correlationDimension((filteredSignal(sensor,1:10*Fs, 4)),eLAG4,eDIM4);
+cDim5 = correlationDimension((filteredSignal(sensor,1:10*Fs, 5)),eLAG5,eDIM5);
 
-
+%% Plot
 
 figure(2);
 subplot(2,3,1);plot3(XX(1:1000,1),XX(1:1000,2),XX(1:1000,3));grid
 title("EEG");
+
 subplot(2,3,2);plot3(XX1(1:1000,1),XX1(1:1000,2),XX1(1:1000,3));grid
-title("Delta");
+title("Delta"); 
+text(0,4,0,"approximateEntropy");text(0,0,0,num2str(aE1));
+text(0,-4,0,"correlationDimension");text(0,-8,0,num2str(cDim1));
+
 subplot(2,3,3);plot3(XX2(1:1000,1),XX2(1:1000,2),XX2(1:1000,3));grid
 title("Theta");
+text(-12,5,0,"approximateEntropy");text(-12,0,0,num2str(aE2));
+text(0,-5,0,"correlationDimension");text(0,-10,0,num2str(cDim2));
+
+
 subplot(2,3,4);plot3(XX3(1:1000,1),XX3(1:1000,2),XX3(1:1000,3));grid
 title("Alpha");
+text(0,5,+15,"approximateEntropy");text(-3,0,+15,num2str(aE3));
+text(0,-5,-20,"correlationDimension");text(0,-10,-23,num2str(cDim3));
+
 subplot(2,3,5);plot3(XX4(1:1000,1),XX4(1:1000,2),XX4(1:1000,3));grid
 title("Beta");
+text(0,5,+15,"approximateEntropy");text(-3,0,+15,num2str(aE4));
+text(0,-5,-20,"correlationDimension");text(0,-10,-23,num2str(cDim4));
+
 subplot(2,3,6);plot3(XX5(1:1000,1),XX5(1:1000,2),XX5(1:1000,3));grid
 title("Gamma");
-
+text(0,4,-5,"approximateEntropy");text(0,0,-5,num2str(aE5));
+text(0,-4,0,"correlationDimension");text(0,-8,0,num2str(cDim5));
 
